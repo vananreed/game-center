8e94b45290adff8d3e742c690a3c0b45aa7691b1
Reed Van Anrooy <reed.vananrooy@gmail.com>
2020-02-21 22:43:58 -0700

    Removes unnecessary specs

--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Achievement, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:criteria) }
+
+  describe '#criteria_met?' do
+
+  end
 end
